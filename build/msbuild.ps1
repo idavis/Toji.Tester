5ca280485b022a598476cb2dd2a8907a85e15f76
Ian Davis <ian@innovatian.com>
2012-02-18 10:56:22 -0800

    cleaning up msbuild execution and adding betting logging

--- build/msbuild.ps1
+++ build/msbuild.ps1
@@ -17,5 +17,8 @@ properties {
 }
 
 Task Invoke-MsBuild {
-  exec { msbuild /m:"$($msbuild.max_cpu_count)" /p:BuildInParralel=$msbuild.build_in_parralel "/logger:$($msbuild.logger);logfile=$($msbuild.logfilepath)\$($msbuild.logfilename)" /p:Configuration="$($build.configuration)" /p:Platform="$($msbuild.platform)" /p:OutDir="$($build.dir)"\\ "$($solution.file)" }
+  $command = "msbuild /m:$($msbuild.max_cpu_count) /p:BuildInParralel=$($msbuild.build_in_parralel) `"/logger:$($msbuild.logger);logfile=$($msbuild.logfilepath)\$($msbuild.logfilename)`" /p:Configuration=`"$($build.configuration)`" /p:Platform=`"$($msbuild.platform)`" /p:OutDir=`"$($build.dir)`"\\ `"$($solution.file)`""
+  $message = "Error executing command: {0}"
+  $errorMessage = $message -f $command
+  exec { msbuild /m:"$($msbuild.max_cpu_count)" "/p:BuildInParralel=$($msbuild.build_in_parralel)" "/logger:$($msbuild.logger);logfile=$($msbuild.logfilepath)\$($msbuild.logfilename)" /p:Configuration="$($build.configuration)" /p:Platform="$($msbuild.platform)" /p:OutDir="$($build.dir)"\\ "$($solution.file)" } $errorMessage
 }
