1a67089858830503f8b371f22bb23239423313bc
Ian Davis <ian@innovatian.com>
2012-02-18 11:13:56 -0800

    reworking build.ps1 for global exit code

--- build.ps1
+++ build.ps1
@@ -1,1 +1,24 @@
-﻿& .\build.cmd @args
+﻿param(
+  [Parameter(Position=0,Mandatory=0)]
+  [string]$buildFile = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\build\build.ps1",
+  [Parameter(Position=1,Mandatory=0)]
+  [string[]]$taskList = @(),
+  [Parameter(Position=2,Mandatory=0)]
+  [string]$framework = '3.5',
+  [Parameter(Position=3,Mandatory=0)]
+  [switch]$docs = $false,
+  [Parameter(Position=4,Mandatory=0)]
+  [System.Collections.Hashtable]$parameters = @{},
+  [Parameter(Position=5, Mandatory=0)]
+  [System.Collections.Hashtable]$properties = @{}
+)
+write-host $buildFile
+$scriptPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+$buildPath = (Resolve-Path $scriptPath\build)
+
+. $buildPath\bootstrap.ps1 $buildPath
+$psakeModule = @(Get-ChildItem $scriptPath\* -recurse -include psake.ps1)[0].FullName
+invoke-psake $buildFile $taskList $framework $docs $parameters $properties
+write-host "exit: $lastexitcode"
+[Environment]::Exit($lastexitcode)
+#exit $lastexitcode
